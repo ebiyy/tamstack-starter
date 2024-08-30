de4f6437680c91263c324e1c9534f1880efd75a5
ebiyy <kuraim22@gmail.com>
2024-08-30 19:36:39 +0900

    generate apps/frontend

---
+++ apps/frontend/app/root.tsx
@@ -0,0 +1,35 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  MetaFunction,
+} from "@remix-run/react";
+import "./tailwind.css";
+
+export const meta: MetaFunction = () => [
+  { charSet: "utf-8" },
+  { name: "viewport", content: "width=device-width, initial-scale=1" },
+  { title: "Your App Title" }, 
+];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
